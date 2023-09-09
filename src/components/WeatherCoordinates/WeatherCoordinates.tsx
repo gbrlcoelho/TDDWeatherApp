1745ab5e7059ba997741bb8895d71f11a1ba6013
gbrlcoelho <coelhogabriel67@gmail.com>
2023-09-09 10:35:31 -0300

    feat(components):weather coordinates component and their tests

--- src/components/WeatherCoordinates/WeatherCoordinates.tsx
+++ src/components/WeatherCoordinates/WeatherCoordinates.tsx
@@ -1,8 +1,92 @@
+import {yupResolver} from '@hookform/resolvers/yup';
+import {useNavigation} from '@react-navigation/native';
 import React from 'react';
-import {View} from 'react-native';
+import {Controller, useForm} from 'react-hook-form';
+import {StyleSheet, TextInput, View} from 'react-native';
+import * as Yup from 'yup';
+import {Colors} from '../../screens/HomeScreen/constants';
+import Button from '../Button/Button';
+
+type FormValues = {
+  latitude: string;
+  longitude: string;
+};
+
+const validationSchema = Yup.object().shape({
+  latitude: Yup.string().required(),
+  longitude: Yup.string().required(),
+});
 
 const WeatherCoordinates = () => {
-  return <View testID="weather-coordinates" />;
+  const {navigate} = useNavigation();
+  const {handleSubmit, control} = useForm<FormValues>({
+    resolver: yupResolver(validationSchema),
+    defaultValues: {
+      latitude: '',
+      longitude: '',
+    },
+    mode: 'onChange',
+  });
+
+  const handleSubmitCoordinates = handleSubmit((values: FormValues) => {
+    navigate('WeatherScreen', {
+      latitude: Number(values.latitude),
+      longitude: Number(values.longitude),
+    });
+  });
+
+  return (
+    <View testID="weather-coordinates">
+      <View style={styles.inputs}>
+        <Controller
+          control={control}
+          name="latitude"
+          render={({field, ...props}) => (
+            <TextInput
+              {...props}
+              testID="weather-coordinates-latitude"
+              onChangeText={field.onChange}
+              style={styles.input}
+              placeholder="Lat"
+              placeholderTextColor={Colors.GRAY}
+            />
+          )}
+        />
+        <Controller
+          control={control}
+          name="longitude"
+          render={({field, ...props}) => (
+            <TextInput
+              {...props}
+              testID="weather-coordinates-longitude"
+              onChangeText={field.onChange}
+              style={styles.input}
+              placeholder="Lon"
+              placeholderTextColor={Colors.GRAY}
+            />
+          )}
+        />
+      </View>
+      <Button label="Find" onPress={handleSubmitCoordinates} />
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  inputs: {
+    flexDirection: 'column',
+    marginBottom: 15,
+  },
+  input: {
+    backgroundColor: Colors.TRANSPARENT,
+    margin: 5,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: Colors.GRAY,
+    paddingHorizontal: 15,
+    paddingVertical: 8,
+    color: Colors.WHITE,
+  },
+});
 
 export default WeatherCoordinates;
--- src/components/WeatherCoordinates/__tests__/WeatherCoordinates.test.tsx
+++ src/components/WeatherCoordinates/__tests__/WeatherCoordinates.test.tsx
@@ -1,11 +1,66 @@
-import {render} from '@testing-library/react-native';
+import {useNavigation} from '@react-navigation/native';
+import {fireEvent, render, waitFor} from '@testing-library/react-native';
 import React from 'react';
 import WeatherCoordinates from '../WeatherCoordinates';
+
+jest.mock('@react-navigation/native', () => {
+  return {
+    ...jest.requireActual<object>('@react-navigation/native'),
+    useNavigation: jest.fn(() => ({navigate: jest.fn()})),
+  };
+});
 
 describe('WeatherCoordinates', () => {
   it('Should render correctly', () => {
     const {getByTestId} = render(<WeatherCoordinates />);
 
     expect(getByTestId('weather-coordinates')).toBeDefined();
+  });
+
+  it('Should navigate to Weather screen with given coordinates when valid form is submitted', async () => {
+    const mockNavigate = jest.fn();
+
+    (useNavigation as jest.Mock).mockReturnValueOnce({navigate: mockNavigate});
+
+    const {getByTestId} = render(<WeatherCoordinates />);
+
+    const fields = {
+      latitude: getByTestId('weather-coordinates-latitude'),
+      longitude: getByTestId('weather-coordinates-longitude'),
+    };
+
+    fireEvent.changeText(fields.latitude, '0');
+    fireEvent.changeText(fields.longitude, '0');
+
+    fireEvent.press(getByTestId('button'));
+
+    await waitFor(() => {
+      expect(mockNavigate).toBeCalledWith('WeatherScreen', {
+        latitude: 0,
+        longitude: 0,
+      });
+    });
+  });
+
+  describe('Latitude field', () => {
+    it('Should not show error when value is the lowest range value', () => {
+      const {getByTestId, findByText} = render(<WeatherCoordinates />);
+
+      fireEvent.changeText(getByTestId('weather-coordinates-latitude'), '-90');
+
+      return expect(
+        findByText('Latitude must be a valid number'),
+      ).rejects.toThrow();
+    });
+
+    it('Should not show error when value is the highest range value', () => {
+      const {getByTestId, findByText} = render(<WeatherCoordinates />);
+
+      fireEvent.changeText(getByTestId('weather-coordinates-latitude'), '90');
+
+      return expect(
+        findByText('Latitude must be a valid number'),
+      ).rejects.toThrow();
+    });
   });
 });
