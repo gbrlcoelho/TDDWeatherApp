b6b095eb4f40bd52a7c38ca0a621bc41d7fb1cb5
gbrlcoelho <coelhogabriel67@gmail.com>
2023-09-09 16:46:17 -0300

    feat(app): added redux provider and finish tests

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
-import {SafeAreaView} from 'react-native';
+import {SafeAreaView, StyleSheet} from 'react-native';
+import {Provider} from 'react-redux';
 import Router from './Routes/Router';
+import {Colors} from './screens/HomeScreen/constants';
+import store from './store';
 
 export const App = () => {
   return (
-    <SafeAreaView style={{flex: 1, backgroundColor: 'black'}} testID="app">
-      <Router />
+    <SafeAreaView style={styles.container} testID="app">
+      <Provider store={store}>
+        <Router />
+      </Provider>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.DARKER_GRAY,
+  },
+});
--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,17 +1,39 @@
 import {render} from '@testing-library/react-native';
 import React from 'react';
 import {View} from 'react-native';
+import {Provider} from 'react-redux';
+
 import {App} from '../App';
 import Router from '../Routes/Router';
+import store from '../store';
 
 jest.mock('../Routes/Router', () => jest.fn());
+jest.mock('react-redux', () => {
+  return {
+    ...jest.requireActual<object>('react-redux'),
+    Provider: jest.fn(),
+  };
+});
 
 describe('App', () => {
-  it('Should render Router', () => {
-    (Router as jest.Mock).mockReturnValueOnce(<View testID="mock-router" />);
+  test('Should render routes', () => {
+    (Provider as jest.Mock).mockImplementationOnce(({children}) => children);
+
+    (Router as jest.Mock).mockReturnValueOnce(<View testID="mock-routes" />);
+    const {getByTestId} = render(<App />);
+    getByTestId('mock-routes');
+  });
+
+  test('Should render Provider', () => {
+    let providerStore!: typeof store;
+
+    (Provider as jest.Mock).mockImplementationOnce(({store: mockStore}) => {
+      providerStore = mockStore;
+      return <View testID="mock-provider" />;
+    });
 
     const {getByTestId} = render(<App />);
-
-    expect(getByTestId('mock-router')).toBeDefined();
+    getByTestId('mock-provider');
+    expect(providerStore).toBe(store);
   });
 });
