8795620f8ac6de1b9e58a8bacd143cc7b2945ef2
gbrlcoelho <coelhogabriel67@gmail.com>
2023-09-11 21:06:00 -0300

    feat(screens): created weather screen and tests

--- src/screens/WeatherScreen/WeatherScreen.tsx
+++ src/screens/WeatherScreen/WeatherScreen.tsx
@@ -1,8 +1,175 @@
-import React from 'react';
-import {View} from 'react-native';
+/* eslint-disable @typescript-eslint/no-shadow */
+import {RouteProp, useNavigation, useRoute} from '@react-navigation/native';
+import React, {useEffect} from 'react';
+import {
+  ActivityIndicator,
+  Image,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import {connect} from 'react-redux';
+import {RootStackParamList} from '../../routes/Router';
+import {StateType} from '../../store/reducers';
+import {fetchWeather} from '../../store/weather/actions';
+import {Status} from '../../types/Status';
+import {WeatherType} from '../../types/Weather';
+import {Colors} from '../HomeScreen/constants';
 
-const WeatherScreen = () => {
-  return <View testID="weather-screen" />;
+type WeatherScreenRouteProp = RouteProp<RootStackParamList, 'WeatherScreen'>;
+
+type StateProps = {
+  weather: WeatherType;
+  status: Status;
+  error: string;
 };
 
-export default WeatherScreen;
+type DispatchProps = {
+  fetchWeather: typeof fetchWeather;
+};
+
+type Props = StateProps & DispatchProps;
+
+const WeatherScreen = (props: Props) => {
+  const {fetchWeather, status, error, weather} = props;
+  const {params} = useRoute<WeatherScreenRouteProp>();
+  const {goBack} = useNavigation();
+
+  useEffect(() => {
+    fetchWeather(params.latitude, params.longitude);
+  }, [fetchWeather, params.latitude, params.longitude]);
+
+  return (
+    <View style={styles.container} testID="weather-screen">
+      <TouchableOpacity style={styles.back} onPress={goBack}>
+        <Text style={styles.backText}>Home</Text>
+      </TouchableOpacity>
+      {status === Status.LOADING && (
+        <ActivityIndicator
+          size="large"
+          color={Colors.DARKER_GRAY}
+          testID="weather-screen-loader"
+        />
+      )}
+      {status === Status.FAILURE && <Text style={styles.error}>{error}</Text>}
+      {status === Status.SUCCESS && (
+        <>
+          <View style={styles.head}>
+            {weather.icon && (
+              <Image
+                testID="weather-screen-icon"
+                style={styles.icon}
+                source={{uri: weather.icon}}
+              />
+            )}
+            <Text style={styles.city}>{weather.city}</Text>
+            {weather.description && (
+              <Text
+                style={styles.description}
+                testID="weather-screen-description">
+                {weather.description}
+              </Text>
+            )}
+          </View>
+          <View style={styles.stats}>
+            <Text style={styles.statsTitle}>Stats</Text>
+            <View
+              testID="weather-screen-temperature"
+              style={[styles.card, styles.cardNegative]}>
+              <Text style={styles.cardTextNegative}>Temperature</Text>
+              <Text style={styles.cardTextNegative}>
+                {Math.round(weather.temperature)}°C
+              </Text>
+            </View>
+            <View testID="weather-screen-wind" style={styles.card}>
+              <Text style={styles.cardText}>Wind</Text>
+              <Text style={styles.cardText}>{weather.windSpeed}m/s</Text>
+            </View>
+            <View
+              testID="weather-screen-humidity"
+              style={[styles.card, styles.cardNegative]}>
+              <Text style={styles.cardTextNegative}>Humidity</Text>
+              <Text style={styles.cardTextNegative}>{weather.humidity}%</Text>
+            </View>
+            <View testID="weather-screen-pressure" style={styles.card}>
+              <Text style={styles.cardText}>Pressure</Text>
+              <Text style={styles.cardText}>{weather.pressure} hPa</Text>
+            </View>
+          </View>
+        </>
+      )}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 20,
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-evenly',
+    backgroundColor: Colors.WHITE,
+  },
+  back: {
+    position: 'absolute',
+    top: 20,
+    left: 20,
+  },
+  backText: {
+    fontSize: 24,
+  },
+  error: {
+    color: Colors.ERROR,
+    textAlign: 'center',
+  },
+  head: {
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  icon: {
+    width: 200,
+    height: 200,
+  },
+  city: {
+    fontSize: 48,
+  },
+  description: {
+    fontSize: 24,
+    textTransform: 'capitalize',
+  },
+  stats: {
+    paddingHorizontal: '10%',
+  },
+  statsTitle: {
+    fontSize: 13,
+    color: Colors.LIGHTER_GRAY,
+    marginBottom: 10,
+  },
+  card: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 15,
+    borderRadius: 10,
+  },
+  cardNegative: {
+    backgroundColor: Colors.BLUE,
+  },
+  cardTextNegative: {
+    color: Colors.WHITE,
+  },
+  cardText: {
+    color: Colors.LIGHTER_GRAY,
+  },
+});
+
+export default connect<StateProps, DispatchProps, {}, StateType>(
+  state => ({
+    status: state.weather.status,
+    weather: state.weather.weather,
+    error: state.weather.error,
+  }),
+  {
+    fetchWeather,
+  },
+)(WeatherScreen);
--- src/screens/WeatherScreen/__tests__/WeatherScreen.test.tsx
+++ src/screens/WeatherScreen/__tests__/WeatherScreen.test.tsx
@@ -1,11 +1,194 @@
-import {render} from '@testing-library/react-native';
+import {useNavigation} from '@react-navigation/native';
 import React from 'react';
+import {
+  fetchWeather,
+  fetchWeatherFailure,
+  fetchWeatherSuccess,
+} from '../../../store/weather/actions';
+import {nullWeather} from '../../../types/Weather';
+import {
+  act,
+  fireEvent,
+  mockStore,
+  render,
+  waitFor,
+} from '../../../utils/test.utils';
 import WeatherScreen from '../WeatherScreen';
+
+jest.mock('@react-navigation/native', () => {
+  return {
+    ...jest.requireActual<object>('@react-navigation/native'),
+    useNavigation: jest.fn().mockReturnValue({goBack: jest.fn()}),
+    useRoute: jest.fn().mockReturnValue({params: {longitude: 0, latitude: 0}}),
+  };
+});
 
 describe('WeatherScreen', () => {
   it('should render correctly', () => {
-    const {getByTestId} = render(<WeatherScreen />);
+    const {getByTestId} = render(<WeatherScreen />, {store: mockStore()});
 
     expect(getByTestId('weather-screen')).toBeDefined();
+  });
+
+  it('Should return to HomeScreen when button home is pressed', () => {
+    const mockGoBack = jest.fn();
+    (useNavigation as jest.Mock).mockReturnValue({goBack: mockGoBack});
+
+    const {getByText} = render(<WeatherScreen />, {store: mockStore()});
+
+    const button = getByText('Home');
+
+    fireEvent.press(button);
+    expect(mockGoBack).toHaveBeenCalled();
+  });
+
+  it('Should fetch weather', async () => {
+    const interceptor = jest.fn();
+    const store = mockStore(interceptor);
+
+    render(<WeatherScreen />, {store});
+
+    await waitFor(() => {
+      expect(interceptor).toHaveBeenCalledWith(fetchWeather(0, 0));
+    });
+  });
+
+  it('Should display loader when fetching weather', async () => {
+    const {getByTestId} = render(<WeatherScreen />, {store: mockStore()});
+    expect(getByTestId('weather-screen-loader')).toBeDefined();
+  });
+
+  it('Should display given error', () => {
+    const store = mockStore();
+    const {getByText} = render(<WeatherScreen />, {store});
+
+    act(() => {
+      store.dispatch(fetchWeatherFailure('mock-error'));
+    });
+
+    expect(getByText('mock-error')).toBeDefined();
+  });
+
+  it('Should display image with given weather icon', () => {
+    const store = mockStore();
+    const {getByTestId} = render(<WeatherScreen />, {store});
+
+    act(() => {
+      store.dispatch(fetchWeatherSuccess({...nullWeather, icon: 'mock-icon'}));
+    });
+
+    const image = getByTestId('weather-screen-icon');
+
+    expect(image).toHaveProp('source', {uri: 'mock-icon'});
+  });
+
+  it('Should not display icon when weather has no icon', () => {
+    const store = mockStore();
+    const {getByTestId} = render(<WeatherScreen />, {store});
+
+    act(() => {
+      store.dispatch(fetchWeatherSuccess(nullWeather));
+    });
+
+    expect(() => getByTestId('weather-screen-icon')).toThrow();
+  });
+
+  it('Should display description from given weather', () => {
+    const store = mockStore();
+    const {getByText} = render(<WeatherScreen />, {store});
+
+    act(() => {
+      store.dispatch(
+        fetchWeatherSuccess({...nullWeather, description: 'mock-description'}),
+      );
+    });
+
+    expect(getByText('mock-description')).toBeDefined();
+  });
+
+  it('Should not display description when weather has no description', () => {
+    const store = mockStore();
+    const {getByTestId} = render(<WeatherScreen />, {store});
+
+    act(() => {
+      store.dispatch(fetchWeatherSuccess(nullWeather));
+    });
+
+    expect(() => getByTestId('weather-screen-description')).toThrow();
+  });
+
+  it('Should display city name from given weather', () => {
+    const store = mockStore();
+    const {getByText} = render(<WeatherScreen />, {store});
+
+    act(() => {
+      store.dispatch(fetchWeatherSuccess({...nullWeather, city: 'mock-city'}));
+    });
+
+    expect(getByText('mock-city')).toBeDefined();
+  });
+
+  it('Should display formatted temperature', () => {
+    const store = mockStore();
+    const {getByTestId, getByText} = render(<WeatherScreen />, {store});
+
+    act(() => {
+      store.dispatch(fetchWeatherSuccess({...nullWeather, temperature: 10.8}));
+    });
+
+    const container = getByTestId('weather-screen-temperature');
+    const title = getByText('Temperature');
+    const temperature = getByText('11°C');
+
+    expect(container).toContainElement(title);
+    expect(container).toContainElement(temperature);
+  });
+
+  it('Should display formatted wind', () => {
+    const store = mockStore();
+    const {getByTestId, getByText} = render(<WeatherScreen />, {store});
+
+    act(() => {
+      store.dispatch(fetchWeatherSuccess({...nullWeather, windSpeed: 1}));
+    });
+
+    const container = getByTestId('weather-screen-wind');
+    const title = getByText('Wind');
+    const wind = getByText('1m/s');
+
+    expect(container).toContainElement(title);
+    expect(container).toContainElement(wind);
+  });
+
+  it('Should display formatted humidity', () => {
+    const store = mockStore();
+    const {getByTestId, getByText} = render(<WeatherScreen />, {store});
+
+    act(() => {
+      store.dispatch(fetchWeatherSuccess({...nullWeather, humidity: 15}));
+    });
+
+    const container = getByTestId('weather-screen-humidity');
+    const title = getByText('Humidity');
+    const humidity = getByText('15%');
+
+    expect(container).toContainElement(title);
+    expect(container).toContainElement(humidity);
+  });
+
+  it('Should display formatted pressure', () => {
+    const store = mockStore();
+    const {getByTestId, getByText} = render(<WeatherScreen />, {store});
+
+    act(() => {
+      store.dispatch(fetchWeatherSuccess({...nullWeather, pressure: 1000}));
+    });
+
+    const container = getByTestId('weather-screen-pressure');
+    const title = getByText('Pressure');
+    const pressure = getByText('1000 hPa');
+
+    expect(container).toContainElement(title);
+    expect(container).toContainElement(pressure);
   });
 });
